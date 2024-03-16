24e957a4e77b2b1b72e346837f466b68b55dcffd
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-03-16 12:19:29 +0200

    "Hero Section Fixed"

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,12 @@
 const Highlights = () => {
   return (
-    <div>Highlights</div>
+    <section id="#highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+      <div className="screen-max-width">
+        <div>
+          <h1 id="title">Get The Highlights.</h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
