942d003d0c8cde14c57890b7d9182e8d1cad0f4e
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-03-17 15:59:59 +0200

    "Models Creation"

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://83ad777e4b3621b84707c3526b3f54f7@o4506926680375296.ingest.us.sentry.io/4506926683914240",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  
+  tracesSampleRate: 1.0, 
+  
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
